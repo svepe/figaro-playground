607ff21601cc508c36797c022015b18ba7587898
Svetlin Penkov <sv.penkov@gmail.com>
2016-04-06 21:25:15 +0100

    Refactor code and improve plotting

--- scripts/regression.py
+++ scripts/regression.py
@@ -18,7 +18,8 @@ class RegressionPlot:
     rospy.init_node('regression_plot')
     self.data_pub = rospy.Publisher('~data', String, queue_size=10)
     self.data = np.empty(shape=(0, 2))
-    self.run_regression = rospy.ServiceProxy('/regression/run', RunRegression2)
+    self.run_regression = rospy.ServiceProxy(
+      '/regression/run', RunRegression2, persistent=True)
 
     self.prior_w0 = Normal2()
     self.prior_w0.mean = 0
@@ -74,85 +75,98 @@ class RegressionPlot:
       self.samples_w0 = resp.samples_w0
       self.samples_w1 = resp.samples_w1
 
+  def plot_data(self):
+    # Plot datapoints and fitted line
+    plt.subplot(3, 1, 1)
+    plt.cla()
+    plt.grid(which='major', color='0.65', linestyle='-')
+    plt.grid(which='minor', color='0.35', linestyle='--')
+    plt.title('$w_0 + w_1 x$')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.gca().set_xlim([-10, 10])
+    plt.gca().set_ylim([-10, 10])
+    plt.scatter(self.data[:, 0], self.data[:, 1])
+    x = np.arange(-10, 10, 0.1)
+    y = self.posterior_w0.mean + self.posterior_w1.mean * x
+    plt.plot(x, y, 'r')
+
+  def plot_prior(self):
+    # Plot prior and sampled weights
+    plt.subplot(3, 1, 2)
+    plt.cla()
+    plt.grid(which='major', color='0.65', linestyle='-')
+    plt.grid(which='minor', color='0.35', linestyle='--')
+    plt.title('Prior')
+    plt.xlabel('$w_0$')
+    plt.ylabel('$w_1$')
+
+    plt.scatter(
+      np.asarray([w.value for w in self.samples_w0]),
+      np.asarray([w.value for w in self.samples_w1]),
+      c=np.asarray([w.weight for w in self.samples_w0]),
+      cmap=plt.cm.get_cmap('RdYlBu_r'))
+
+    l = plt.cm.ScalarMappable(cmap=plt.cm.get_cmap('RdYlBu_r'))
+    l.set_array(
+      [w.weight for w in self.samples_w0 if np.isfinite(w.weight)])
+    cb = plt.colorbar(l)
+    cb.set_label('Sample likelihood')
+
+    (xmin, xmax) = plt.gca().get_xlim()
+    (ymin, ymax) = plt.gca().get_ylim()
+    pdf_x, pdf_y = np.meshgrid(
+      np.arange(xmin, xmax, 0.1), np.arange(ymin, ymax, 0.1))
+    pdf = mlab.bivariate_normal(
+      pdf_x, pdf_y,
+      math.sqrt(self.prior_w0.variance), math.sqrt(self.prior_w1.variance),
+      self.prior_w0.mean, self.prior_w1.mean, sigmaxy=0.0)
+    plt.contour(pdf_x, pdf_y, pdf)
+
+  def plot_posterior(self):
+    plt.subplot(3, 1, 3)
+    plt.cla()
+    plt.grid(which='major', color='0.65', linestyle='-')
+    plt.grid(which='minor', color='0.35', linestyle='--')
+    plt.title('Posterior')
+    plt.xlabel('$w_0$')
+    plt.ylabel('$w_1$')
+
+    plt.gca().set_xlim(self.fig.axes[1].get_xlim())
+    plt.gca().set_ylim(self.fig.axes[1].get_ylim())
+
+    (xmin, xmax) = plt.gca().get_xlim()
+    (ymin, ymax) = plt.gca().get_ylim()
+
+    pdf_x, pdf_y = np.meshgrid(
+      np.arange(xmin, xmax, 0.1), np.arange(ymin, ymax, 0.1))
+
+    pdf = mlab.bivariate_normal(
+      pdf_x, pdf_y,
+      math.sqrt(self.posterior_w0.variance),
+      math.sqrt(self.posterior_w1.variance),
+      self.posterior_w0.mean,
+      self.posterior_w1.mean,
+      sigmaxy=0.0)
+
+    plt.contour(pdf_x, pdf_y, pdf)
+
+    l = plt.cm.ScalarMappable(cmap=plt.cm.get_cmap('RdYlBu_r'))
+    l.set_array(pdf)
+    cb = plt.colorbar(l)
+    cb.set_label('Posterior PDF')
+
   def loop(self):
       plt.ion()
 
       self.fig = plt.figure()
       self.fig.canvas.mpl_connect('button_press_event', self.onclick)
       while not rospy.is_shutdown():
-
-        # Plot datapoints and fitted line
-        plt.subplot(3, 1, 1)
-        plt.cla()
-        plt.title('$w_0 + w_1 x$')
-        plt.xlabel('x')
-        plt.ylabel('y')
-        plt.gca().set_xlim([-10, 10])
-        plt.gca().set_ylim([-10, 10])
-        plt.scatter(self.data[:, 0], self.data[:, 1])
-        x = np.arange(-10, 10, 0.1)
-        y = self.posterior_w0.mean + self.posterior_w1.mean * x
-        plt.plot(x, y, 'r')
-
-        # Plot prior and sampled weights
-        plt.subplot(3, 1, 2)
-        plt.cla()
-        plt.title('Prior')
-        plt.xlabel('$w_0$')
-        plt.ylabel('$w_1$')
-
-        plt.scatter(
-          np.asarray([w.value for w in self.samples_w0]),
-          np.asarray([w.value for w in self.samples_w1]),
-          c=np.asarray([w.weight for w in self.samples_w0]),
-          cmap=plt.cm.get_cmap('RdYlBu_r'))
-
-        l = plt.cm.ScalarMappable(cmap=plt.cm.get_cmap('RdYlBu_r'))
-        l.set_array(
-          [w.weight for w in self.samples_w0 if np.isfinite(w.weight)])
-        cb = plt.colorbar(l)
-        cb.set_label('Sample likelihood')
-
-        (xmin, xmax) = plt.gca().get_xlim()
-        (ymin, ymax) = plt.gca().get_ylim()
-        pdf_x, pdf_y = np.meshgrid(
-          np.arange(xmin, xmax, 0.1), np.arange(ymin, ymax, 0.1))
-        pdf = mlab.bivariate_normal(
-          pdf_x, pdf_y,
-          math.sqrt(self.prior_w0.variance), math.sqrt(self.prior_w1.variance),
-          self.prior_w0.mean, self.prior_w1.mean, sigmaxy=0.0)
-        plt.contour(pdf_x, pdf_y, pdf)
-
-        # Plot posterior
-        plt.subplot(3, 1, 3)
-        plt.cla()
-        plt.title('Posterior')
-        plt.xlabel('$w_0$')
-        plt.ylabel('$w_1$')
-
-        plt.gca().set_xlim(self.fig.axes[1].get_xlim())
-        plt.gca().set_ylim(self.fig.axes[1].get_ylim())
-
-        (xmin, xmax) = plt.gca().get_xlim()
-        (ymin, ymax) = plt.gca().get_ylim()
-
-        pdf_x, pdf_y = np.meshgrid(
-          np.arange(xmin, xmax, 0.1), np.arange(ymin, ymax, 0.1))
-
-        pdf = mlab.bivariate_normal(
-          pdf_x, pdf_y,
-          math.sqrt(self.posterior_w0.variance),
-          math.sqrt(self.posterior_w1.variance),
-          self.posterior_w0.mean,
-          self.posterior_w1.mean,
-          sigmaxy=0.0)
-
-        plt.contourf(pdf_x, pdf_y, pdf)
-        cb = plt.colorbar()
-        cb.set_label('Posterior PDF')
-
+        self.plot_data()
+        self.plot_prior()
+        self.plot_posterior()
         plt.show()
-        plt.pause(0.01)
+        plt.pause(0.1)
         self.fig.clear()
 
 if __name__ == '__main__':
